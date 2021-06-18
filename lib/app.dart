72f0d889108028868c1b358ea12000f6e5fb90aa
Mohamed Elshaarawy <elshaarawy200825@hotmail.com>
2021-06-18 16:50:33 +0200

    add firebase init in app

--- lib/app.dart
+++ lib/app.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:smart_poultry_farm/screens/home.dart';
@@ -6,17 +7,27 @@ import 'package:smart_poultry_farm/screens/splash.dart';
 class SPFApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    String title ;
-    if(kIsWeb){
-      title = "SPF From Web";
-    }else{
-      title = "SPF From Mobile";
-    }
     return MaterialApp(
       theme: ThemeData(
         primarySwatch: Colors.green,
       ),
-      home: Splash(),
+      home: FutureBuilder<Widget>(
+        initialData: Splash(),
+        future: findHome(),
+        builder: (context, ss) => ss.data,
+      ),
     );
+  }
+
+  Future<Widget> findHome() async {
+    String title;
+    if (kIsWeb) {
+      title = "SPF From Web";
+    } else {
+      title = "SPF From Mobile";
+    }
+    await Firebase.initializeApp();
+    await Future.delayed(Duration(seconds: 3));
+    return Home(title);
   }
 }
